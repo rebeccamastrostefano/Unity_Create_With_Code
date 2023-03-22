f7ed8b5589fa18d57dd1ddfda967e517d068f406
rebeccamastrostefano <115167494+rebeccamastrostefano@users.noreply.github.com>
2023-03-22 12:47:58 +0100

    Unit1 Complete!

--- GameDev/Unit1/Unit1/Assets/Scripts/FollowPlayer.cs
+++ GameDev/Unit1/Unit1/Assets/Scripts/FollowPlayer.cs
@@ -8,14 +8,15 @@ public class FollowPlayer : MonoBehaviour
     private Vector3 offset = new Vector3(0, 5, -7); //new because it creates a new object, a vector, which is an arrow
 
     // Start is called before the first frame update
-    void Start()
+     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // LateUpdate is called once per frame after every Update is called
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset; 
+        
     }
 }
--- GameDev/Unit1/Unit1/Assets/Scripts/PlayerControl.cs
+++ GameDev/Unit1/Unit1/Assets/Scripts/PlayerControl.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed= 5.0f;
-    
-    // Start is called before the first frame update 
+    //Max forward/backward spees in m/s
+    private float speed= 20.0f;
+
+    //Max rotate speed in degrees
+    private float turnSpeed= 45.0f;
+
+    //Input to turn vehicle
+    private float horizontalInput;
+
+    //Input to make vehicle move forward/backward
+    private float forwardInput;
+
+    //Start is called before the first frame update 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
-    {
+    {   // horizontal and Vertical Input are in the [1, -1] range
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        forwardInput = Input.GetAxis("Vertical");
+
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Turn vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        // Move vehicle sideways
+        // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
         
     }
 }
